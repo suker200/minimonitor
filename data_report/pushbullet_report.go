09da8fca8785265eec74047e5da7dfd01f12fc14
suker200 <tan.luong1989@gmail.com>
2015-09-18 22:07:29 +0700

    update fix pushbullet parser, disk metric

--- data_report/pushbullet_report.go
+++ data_report/pushbullet_report.go
@@ -2,23 +2,52 @@ package data_report
 
 import (
 	"bytes"
+	// "encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-func pushbullet_report() {
-	uri_path := "https://api.pushbullet.com/v2/pushes"
-	var query = []byte(`{"type": "note", "title": "Note Title", "body": "Note Body", "email": "xxxxxxxx"}`)
-	req, err := http.NewRequest("POST", uri_path, bytes.NewBuffer(query))
-	req.Header.Set("Access-Token", "xxxxxxx")
-	req.Header.Set("Content-Type", "application/json")
-	if err != nil {
-		panic(err)
-		fmt.Println(err)
+func Pushbullet_report(email string, title_message string, result string, warning int, critical int) {
+	// convert result value to int
+	var notification string
+	var check bool
+	var re = regexp.MustCompile(`[\n%]`)
+	// result_int, err := strconv.Atoi(strings.Replace(result, "\n", "", -1))
+	result_int, err := strconv.Atoi(re.ReplaceAllString(result, ""))
+	if result_int > critical && err == nil {
+		// critical case, send notification
+		notification = "[CRITICAL] " + title_message + " " + strings.Replace(result, "\n", "", -1) + " greater than " + strconv.Itoa(critical)
+		// data_report.Pushbullet_report(object_config["email"]["email"].(string), "CCU", notification)
+		check = true
+	} else if result_int > warning && err == nil {
+		// warning case, send notification
+		notification = "[WARNING] " + title_message + " " + strings.Replace(result, "\n", "", -1) + " greater than " + strconv.Itoa(warning)
+		// data_report.Pushbullet_report(object_config["email"]["email"].(string), "CCU", notification)
+		check = true
 	}
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+
+	if check {
+		list_email := strings.Split(email, ",")
+		for _, reciever := range list_email {
+			uri_path := "https://api.pushbullet.com/v2/pushes"
+			email_reciever := reciever
+			message := "{'type': 'note', 'title':'" + title_message + "', 'body':'" + notification + "', 'email':'" + email_reciever + "'}"
+			message_format := strings.Replace(message, "'", "\"", -1)
+			req, err := http.NewRequest("POST", uri_path, bytes.NewBuffer([]byte(message_format)))
+			req.Header.Set("Access-Token", "xxx")
+			req.Header.Set("Content-Type", "application/json")
+			if err != nil {
+				panic(err)
+				fmt.Println(err)
+			}
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			response, _ := ioutil.ReadAll(resp.Body)
+			fmt.Println(string(response))
+		}
+	}
 }
