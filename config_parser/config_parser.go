2f43e46bd8c98612bc5c6cbb7927e63b0cea149c
suker200 <tan.luong1989@gmail.com>
2015-09-20 01:53:41 +0700

    update config_parser

--- config_parser/config_parser.go
+++ config_parser/config_parser.go
@@ -54,36 +54,32 @@ func Config_parser() (map[string]map[string]interface{}, Server, bool, []uint8, 
 	check_md5 = hash.Sum(file)
 
 	data_file := string(file)
-	// data_file_2 := strings.Split(data_file, "\n")
 
+	//Get config information by hostname from config_file
 	var re = regexp.MustCompile(server_name_2[0] + "-.*")
-
-	for _, data := range re.FindAllString(data_file, -1) {
-		data_tmp := strings.Split(data, "-")
-		if data_tmp[0] == server.Name {
-			check_config = true
-			server.Tag = data_tmp[1]
-			// var feature Feature
-			for element := 2; element < len(data_tmp); element++ {
-				data_parser := make(map[string]interface{})
-				element_array := strings.Split(data_tmp[element], ":")
-				check_value_list := strings.Split(element_array[1], ",")
-				// Case custom format, email:tan.luong1989@gmail.com,x.lhtan@yahoo.com <-- has "," delimiter but not welknown format
-				check_format := strings.Split(check_value_list[1], "=")
-				if len(check_value_list) == 1 || len(check_format) == 1 {
-					data_parser[element_array[0]] = element_array[1]
-				} else {
-					list_monitor = append(list_monitor, element_array[0])
-					for count := 0; count < len(check_value_list); count++ {
-						temp_value := strings.Split(check_value_list[count], "=")
-						data_parser[temp_value[0]], err = strconv.Atoi(temp_value[1])
-					}
+	data := re.FindAllString(data_file, -1)
+	data_tmp := strings.Split(data[0], "-")
+	if data_tmp[0] == server.Name {
+		check_config = true
+		server.Tag = data_tmp[1]
+		for element := 2; element < len(data_tmp); element++ {
+			data_parser := make(map[string]interface{})
+			element_array := strings.Split(data_tmp[element], ":")
+			check_value_list := strings.Split(element_array[1], ",")
+			// Case custom format, email:tan.luong1989@gmail.com,x.lhtan@yahoo.com <-- has "," delimiter but not welknown format
+			check_format := strings.Split(check_value_list[1], "=")
+			if len(check_value_list) == 1 || len(check_format) == 1 {
+				data_parser[element_array[0]] = element_array[1]
+			} else {
+				list_monitor = append(list_monitor, element_array[0])
+				for count := 0; count < len(check_value_list); count++ {
+					temp_value := strings.Split(check_value_list[count], "=")
+					data_parser[temp_value[0]], err = strconv.Atoi(temp_value[1])
 				}
-				object_feature[element_array[0]] = data_parser
 			}
-			fmt.Println(object_feature)
+			object_feature[element_array[0]] = data_parser
 		}
+		fmt.Println(object_feature)
 	}
-
 	return object_feature, server, check_config, check_md5, list_monitor
 }
